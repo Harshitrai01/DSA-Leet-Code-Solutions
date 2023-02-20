69065f9b3501cb199768badc075886dae49ebee7
Harshitrai01 <90034825+Harshitrai01@users.noreply.github.com>
2023-02-20 19:45:52 +0530

    Time: 7 ms (89.14%), Space: 13.9 MB (93.57%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    
-    void solve(vector<vector<int>>& image,int i,int j,int color,int m,int n,int           initial){
+    int d_row[4]={-1,0,1,0};
+    int d_col[4]={0,1,0,-1};
+    void solve(vector<vector<int>>& image,int i,int j,int color,int m,int n,int initial){
         if(i<0 or j<0 or i>=m or j>=n or image[i][j]==color)
             return;
         if(image[i][j]==initial){
             image[i][j]=color;
-            
-            int d_row[4]={-1,0,1,0};
-            int d_col[4]={0,1,0,-1};
             
             for(int k=0;k<4;k++){
                 int n_row=i+d_row[k];
