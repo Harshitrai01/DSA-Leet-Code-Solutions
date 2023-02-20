c1b470f7654474fabba6b66ea54e9d21dc656c9f
Harshitrai01 <90034825+Harshitrai01@users.noreply.github.com>
2023-02-20 19:43:29 +0530

    Time: 18 ms (14.10%), Space: 14 MB (53.53%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
     
-    void solve(vector<vector<int>>& image,int i,int j,int color,int m,int n,int           initial,vector<vector<bool>>&vis){
-        
-        if(image[i][j]==initial and vis[i][j]==false){
-            vis[i][j]=true;
+    void solve(vector<vector<int>>& image,int i,int j,int color,int m,int n,int           initial){
+        if(i<0 or j<0 or i>=m or j>=n or image[i][j]==color)
+            return;
+        if(image[i][j]==initial){
             image[i][j]=color;
             
-            // left
-            if(j!=0){
-                solve(image,i,j-1,color,m,n,initial,vis);
-            }
+            int d_row[4]={-1,0,1,0};
+            int d_col[4]={0,1,0,-1};
             
-            // right
-            if(j!=n-1){
-                solve(image,i,j+1,color,m,n,initial,vis);
+            for(int k=0;k<4;k++){
+                int n_row=i+d_row[k];
+                int n_col=j+d_col[k];
+                solve(image,n_row,n_col,color,m,n,initial);
             }
-            
-            // up
-            if(i!=0){
-                solve(image,i-1,j,color,m,n,initial,vis);
-            }
-            
-            // down
-            if(i!=m-1){
-                solve(image,i+1,j,color,m,n,initial,vis);
-            }
-            return;
-            
         }
-        else
-            return;
         
     }
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int m=image.size();
         int n=image[0].size();
-        vector<vector<bool>>vis(m,vector<bool>(n,false));
         int initial=image[sr][sc];
-        solve(image,sr,sc,color,m,n,initial,vis);
+        solve(image,sr,sc,color,m,n,initial);
         return image;
     }
 };
