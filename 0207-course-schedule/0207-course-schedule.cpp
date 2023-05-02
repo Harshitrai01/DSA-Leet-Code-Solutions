85dd6d17e8f43ae2eceb24010637cd8de8b5afec
Harshit Kumar Rai <90034825+Harshitrai01@users.noreply.github.com>
2023-05-03 01:33:58 +0530

    Time: 25 ms (45.08%), Space: 13.7 MB (63.81%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canFinish(int N, vector<vector<int>>& prerequisites) {
+	    vector<int> adj[N];
+	    for(auto i: prerequisites){
+	        adj[i[0]].push_back(i[1]);
+	    }
+	    
+	    vector<int>inDeg(N,0);
+	    
+	    for(int i=0;i<N;i++){
+	        for(auto j :adj[i]){
+	            inDeg[j]++;
+	        }
+	    }
+	    
+	    queue<int>q;
+	    for(int i=0;i<N;i++){
+	        if(inDeg[i]==0) q.push(i);
+	    }
+	    
+	    int count=0;
+	    while(!q.empty()){
+	        count++;
+	        int u=q.front();
+	        q.pop();
+	        for(auto x:adj[u]){
+	            inDeg[x]--;
+	            if(inDeg[x]==0)
+	                q.push(x);
+	        }
+	    }
+	    
+	    if(count==N) return true;
+	    return false;
+    }
+};
