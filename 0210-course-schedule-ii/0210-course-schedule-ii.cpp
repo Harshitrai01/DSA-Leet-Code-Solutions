3d6afeb9fdbf2270dbef1d58f355cd753cd12fe2
Harshit Kumar Rai <90034825+Harshitrai01@users.noreply.github.com>
2023-05-03 01:38:57 +0530

    Time: 29 ms (22.79%), Space: 13.7 MB (67.17%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> findOrder(int N, vector<vector<int>>& prerequisites) {
+	    vector<int> adj[N];
+	    for(auto i: prerequisites){
+	        adj[i[1]].push_back(i[0]);
+	    }
+	    
+	    vector<int>inDeg(N,0);
+	    
+	    for(int i=0;i<N;i++){
+	        for(auto j :adj[i]){
+	            inDeg[j]++;
+	        }
+	    }
+	    
+	    queue<int>q;
+	    for(int i=0;i<N;i++){
+	        if(inDeg[i]==0) q.push(i);
+	    }
+	    
+	    int count=0;
+	    vector<int>ans;
+	    
+	    while(!q.empty()){
+	        count++;
+	        int u=q.front();
+	        q.pop();
+	        ans.push_back(u);
+	        for(auto x:adj[u]){
+	            inDeg[x]--;
+	            if(inDeg[x]==0)
+	                q.push(x);
+	        }
+	    }
+	    
+	    if(count!=N) ans.clear();
+	    return ans; 
+    }
+};
